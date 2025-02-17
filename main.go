406e2fb6fca64a6278062a59616b20edb84e6acc
Robert Kroeger <rjkroege@liqui.org>
2025-02-17 20:24:00 +0900

    fixed the kong path for working bindeps

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ var CLI struct {
 	} `cmd help:"print mk vars"`
 
 	Bindeps struct {
-		Args []string `arg:"" name:"args" help:"Packages to generate mk constructs for"`
+		Args []string `arg:"" name:"args" help:"Deps directory and then packages for which to generate mk dependency data."`
 	} `cmd help:"generate binary deps for mk"`
 	
 }
@@ -62,7 +62,7 @@ func main() {
 	case "bindeps <args>":
 		log.Println("mkconfig should generate deps", CLI.Bindeps.Args)
 		// TODO(rjk): This API surface should be fixed at sometime.
-		if err := genBinDeps("bindeps", CLI.Bindeps.Args); err != nil {
+		if err := genBinDeps(CLI.Bindeps.Args[0], CLI.Bindeps.Args[1:]); err != nil {
 			log.Fatalf("can't generate deps %v", err)
 		}
 	}
