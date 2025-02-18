df82b9c548b426500df2746cd6b8f5ad38a892c2
Robert Kroeger <rjkroege@liqui.org>
2025-02-18 15:10:26 +0900

    minor tweak to kong-conversion, renamed installbintargets to install

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ var CLI struct {
 		Scriptspath string `arg:"" name:"scriptspath" help:"pull configuration to this dir"`
 	} `cmd help:"setup an account on an GCP node"`
 
-	Installbintargets struct {
+	Install struct {
 		Targetpath string `arg:"" name:"targetpath" help:"install binaries here"`
 		Args []string `arg:"" name:"args" help:"Binaries to install."`
 	} `cmd help:"Install binaries"`
@@ -84,8 +84,10 @@ func main() {
 		}
 		// TODO(rjk): This feature would become obsolete once I switch to new setup/build scheme.
 		// Isn't this now?
-	case "installbintargets <targetpath> <scriptspath>":
-		if err := InstallBinTargets(CLI.Installbintargets.Targetpath, CLI.Installbintargets.Args); err != nil {
+	case "install <targetpath> <scriptspath>":
+		// I feel that this feature is no longer in use.
+		log.Println("installing...")
+		if err := InstallBinTargets(CLI.Install.Targetpath, CLI.Install.Args); err != nil {
 			log.SetOutput(os.Stderr)
 			log.Fatalf("can't install targets: %v", err)
 		}
